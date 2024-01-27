988d482fc3f9b99bad5391391d71547c1cbb7fb4
Tmarc007 <thomasmarcello@gmail.com>
2024-01-27 10:16:31 -0800

    temperature_v1

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Script");
+
+var userName="George";// creating and assigning value to the var
+
+userName = "James"; //changing the var value
+
+
+console.log(userName);
+
+var employeeName="Kory";
+var employeeAge=27;
+var employeeCountry="USA";
+var employeePhone="5558965";
+var employeeSalaryMonthly=20000;
+var employeeSalaryYearly=employeeSalaryMonthly *12;
+
+var videoTitle="URL";
+var backgroundImage="Clickable URL (same as aboove)";
+var videoChannel="URL to channel";
+var views="upticking number";
+var postedDate="changes daily with date";
+var thumbsUp="upticking thumbs up/down buttons";
+
+
+document.write(
+    `
+    <h3>Employee Info</h3>
+    <p>Name: ${employeeName} </P>
+    <p>Age: ${employeeAge} </p>
+<br><br>
+    <h3>YouTube Variables</h3>
+    <p>Title: ${videoTitle}</p>
+    <p>Image Thumbnail: ${backgroundImage}</p>
+    <p>Video Channel: ${videoChannel}</p>
+    <p>Views: ${views}</p>
+    <p>Date Posted: ${postedDate}</p>
+    <p>Thumbs Up${thumbsUp}</p>
+    
+
+
+    `
+
+);
